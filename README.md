7c1f58fd343addaf3af7c5ca8f7f8675dd1920c3
xiaosheng <xiaosheng.tian@uisee.com>
2022-11-12 19:02:53 +0800

    add description for script

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # auto_launch
+
+## script 1
+file_name: mycm.sh
+description: 
+* 1->complie the uos
+* 2->encrypted json file 
+* 3->rm origin json file
+* 4->launch uos
+
+## script 2
+file_name: mybin.sh
+description: 
+* 1->complie the uos
+* 2->encrypted json file 
+* 3->rm origin json file
+* 4->kill uos_planner
+* 5->. set_env.sh
+* 6->start uos_planner
+
+
+
--- mybin.sh
+++ mybin.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 
-# compile 
+#===============Version 1.0====================
+# file_name:
+#    mybin.sh
+#----------------------------------------------
+# description: 
+#    1->compile uos
+#    2->kill uos_planner
+#    3->waiting uos_planner being killed
+#    4->start uos_planner
+#----------------------------------------------
+# updated date:
+#    2022-11-12
+#----------------------------------------------
+# auther:
+#    tianxiaosheng
+#---------------------------------------------
+# email:
+#    tianxiaoshengbjut@126.com
+#==============================================
+
+# 1.compile 
 cd ../
 catkin_make install
 cd install
 ./encrypt_param.sh
 rm etc/*.json
 
-# kill uos_planner
+# 2.kill uos_planner
 echo "killing uos_planner..."
 killall uos_planner
 
-# waiting being killed
+# 3.waiting being killed
 for ((i=1; i<=20; i++))
 do
     PID=`ps aux | grep uos_planner | awk '{print $2}'`
@@ -27,6 +47,7 @@ do
     fi
 done
 
+# 4.start uos_planner
 . set_env.sh
 echo "start uos_planner"
 bin/uos_planner
